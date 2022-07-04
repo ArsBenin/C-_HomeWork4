b5b4ca07fbefcd2a2ff7509694dc6bf952bdd363
Arseniy <aben@yandex.ru>
2022-07-05 00:04:28 +0300

    Выполнена задача №27

--- Ex.27/Program.cs
+++ Ex.27/Program.cs
@@ -4,3 +4,39 @@
 82 -> 10
 9012 -> 12
 */
+
+int GetNumber()
+{
+    int result = 0;
+    Console.WriteLine("Введите число:");
+    while(true)
+    {
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не натурально число. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+int GetSumm(int number)
+{
+int result = 0;
+while(number>0)
+{
+    result=result+(number%10);
+    number=number/10;
+}
+return result;
+}
+
+int number = GetNumber();
+int result = GetSumm(number);
+
+Console.WriteLine($"Сумма цифр числа {number} = {result}");
