f1ad7e1afa342d8cb73cb18c49181ad6fe8936f6
Arseniy <aben@yandex.ru>
2022-07-05 10:15:07 +0300

    Выполнена задача №25

--- Ex.25/Program.cs
+++ Ex.25/Program.cs
@@ -3,3 +3,36 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не натурально число";
+
+    while(true)
+    {
+        
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+    return result;
+}
+
+
+int numberA = GetNumber("Введите число A");
+int numberB = GetNumber("Введите число B");
+int result = 1;
+
+for (int i = 1; i <= numberB; i++)
+{
+    result = result * numberA;
+}
+
+Console.WriteLine($"{numberA} в степени {numberB} = {result}");
